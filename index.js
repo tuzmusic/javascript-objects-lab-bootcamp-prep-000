919c945c79ff0a6c2e49cd2efb5c725247e97acd
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-03 15:25:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,4 +12,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, object)
   delete object[key]
+  return object
 }
